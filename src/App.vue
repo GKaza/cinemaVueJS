ac3a8bfd7fd31c880cb8f3eab73a7508d3a6e0e9
GKaza <jkazakidis@gmail.com>
2020-05-14 23:51:06 +0200

    data

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <Seat msg="Welcome to Vue Cinema" />
 
+    <table>
+      <tr>
+        <td></td>
+      </tr>
+    </table>
+
     <button id="bookSeats" onclick="bookSeats()">Book your seats</button>
     <div class="displayerBoxes">
       <table class="Displaytable">
@@ -28,10 +34,57 @@
 import Seat from "./components/Seat.vue";
 // import swal from "sweetalert";
 
+const rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+let seats = new Map();
+// let set = [];
+let reserved = JSON.parse(localStorage.getItem("reserved"));
+let max = new Map();
+let maxRow = 0;
+
+// localStorage.removeItem("reserved")
+
+for (let i = 0; i < rows.length; i += 1) {
+  max.set(rows[i], 20);
+}
+
+for (let r = 0; r < 10; r += 1) {
+  for (let x = 1; x < 21; x += 1) {
+    let obj = {
+      id: rows[r] + x,
+      row: rows[r],
+      number: x,
+      available: true
+    }
+
+    seats.set(obj["id"], obj)
+  }
+}
+
+if (reserved) {
+  for (let i = 0; i < reserved.length; i += 1) {
+    let sget = seats.get(reserved[i]);
+    sget.available = false;
+    max.set(sget.row, max.get(sget.row) - 1);
+  }
+} else {
+  reserved = [];
+}
+
+for (let i = 0; i < rows.length; i++) {
+  if (max.get(rows[i]) > maxRow) {
+    maxRow = max.get(rows[i]);
+  }
+}
+
+//
+
+
 export default {
   name: "App",
   components: {
     Seat
+  },
+  data() {
   }
 };
 </script>
